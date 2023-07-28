9eb98abdf10ae97abb9b8eaf11bd9324a9974109
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-28 15:48:21 +0200

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -1,25 +1,19 @@
 import streamlit as st
-import pandas as pd
+
 from streamlit_chat import message
-
-liste = pd.Series(['hallo', 'was'])
-
 
 def on_input_change():
     user_input = st.session_state.user_input
     st.session_state.responses.append(user_input)
-        
 
 def on_btn_click():
     del st.session_state['questions']
     del st.session_state['responses']
 
-
 st.session_state.setdefault('questions', [])
 
 st.title("Survey QA Bot")
-questions_list = ['''I would like to recommend you some Boardgames. 
-Do you have a favorite one?''', 'How many Games should i recommend for you?']
+questions_list = ['q1', 'question 2', 'question 3']
 
 if 'responses' not in st.session_state.keys():
     st.session_state.questions.extend(questions_list)
@@ -34,6 +28,10 @@ with st.container():
     for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
         message(response, is_user = True)
         message(question)
+
+
+with st.container():
+    st.text_input("User Response:", on_change=on_input_change, key="user_input")
 
 
 with st.container():
