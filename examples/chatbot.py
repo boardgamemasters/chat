5cb02b20b8dc280dc5499ddd542ad51c3fb20819
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-28 16:41:34 +0200

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -1,6 +1,10 @@
 import streamlit as st
-
+import sleep from time
+import pandas as pd
 from streamlit_chat import message
+
+games = pd.Series(['lama', 'cow', 'lama', 'beetle', 'lama',
+               'hippo'])
 
 def on_input_change():
     user_input = st.session_state.user_input
@@ -35,63 +39,50 @@ st.button("Clear message", on_click=on_btn_click)
 message(st.session_state.questions[0]) 
 
 with st.container():
-    for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
-        message(response, is_user = True)
-        message(response)
-        message(question)
+    selecthor = 0
+    while 1==1:
+        if selecthor == 0:
+            message(response, is_user = True)
+            if games.isin([response]):
+                sel_game = response
+                selecthor = 1
+            else:
+                message(st.session_state.questions[1])
+        if selecthor == 1:
+            message(st.session_state.questions[2])
+            message(response2, is_user = True)
+            if response2.isnumeric():
+                alt = response2
+                selecthor = 2
+            else:
+                message('Please enter a numeric value')
+        if selecthor== 2:
+            message(f'''Your favorite boardgame is {sel_game}.
+            And you would like to get {alt} recommendations for similar games.
+            Is that correct?
+            (y) , (n)''')
+            selecthor = 3
+        if selecthor== 3:
+            message(response3, is_user = True)
+            if (pd.Series(['y', 'Y', 'yes', 'Yes'])).isin([selecthor3]):
+                message('I can recommend you the following games:')
+            elif (pd.Series(['n', 'N', 'no', 'No'])).isin([selecthor3]):
+                message('Lets try again')
+                selecthor = 0
+            else:
+                message(f'''{response3} is not a valid input. Please try again
+                What is your favorite Boardgame?''')
+                
+                
+                     
+                   
+    
+    # for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
+    #     message(response, is_user = True)
+    #     message(response)
+    #     message(question)
 
 
 with st.container():
     st.text_input("User Response:", on_change=on_input_change, key="user_input")
 
-
-# import streamlit as st
-# from streamlit_chat import message
-# import requests
-# from streamlit.components.v1 import html
-
-# st.set_page_config(
-#     page_title="Streamlit Chat - Demo",
-#     page_icon=":robot:"
-# )
-
-# API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-# # headers = {"Authorization": st.secrets['api_key']}
-
-# st.header("Streamlit Chat - Demo")
-# st.markdown("[Github](https://github.com/ai-yash/st-chat)")
-
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
-
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
-
-# def query(payload):
-# 	response = requests.post(API_URL, json=payload)	#	headers=headers, 
-# 	return response.json()
-
-# def get_text():
-#     input_text = st.text_input("You: ","Hello, how are you?", key="input")
-#     return input_text 
-
-
-# user_input = get_text()
-
-# if user_input:
-#     output = query({
-#         "inputs": {
-#             "past_user_inputs": st.session_state.past,
-#             "generated_responses": st.session_state.generated,
-#             "text": user_input,
-#         },"parameters": {"repetition_penalty": 1.33},
-#     })
-
-#     st.session_state.past.append(user_input)
-#     st.session_state.generated.append(output["generated_text"])
-
-# if st.session_state['generated']:
-
-#     for i in range(len(st.session_state['generated'])-1, -1, -1):
-#         message(st.session_state["generated"][i], key=str(i))
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
