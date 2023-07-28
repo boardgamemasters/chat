b37526381c0d62630a23395213826a39c25b5feb
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-28 14:50:53 +0200

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -1,51 +1,87 @@
 import streamlit as st
+
 from streamlit_chat import message
-import requests
-from streamlit.components.v1 import html
 
-st.set_page_config(
-    page_title="Streamlit Chat - Demo",
-    page_icon=":robot:"
-)
+def on_input_change():
+    user_input = st.session_state.user_input
+    st.session_state.responses.append(user_input)
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-# headers = {"Authorization": st.secrets['api_key']}
+def on_btn_click():
+    del st.session_state['questions']
+    del st.session_state['responses']
 
-st.header("Streamlit Chat - Demo")
-st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+st.session_state.setdefault('questions', [])
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+st.title("Survey QA Bot")
+questions_list = ['question 1', 'question 2', 'question 3']
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+if 'responses' not in st.session_state.keys():
+    st.session_state.questions.extend(questions_list)
+    st.session_state.responses = []
 
-def query(payload):
-	response = requests.post(API_URL, json=payload)	#	headers=headers, 
-	return response.json()
+chat_placeholder = st.empty()
+st.button("Clear message", on_click=on_btn_click)
 
-def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
-    return input_text 
+message(st.session_state.questions[0]) 
+
+with st.container():
+    for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
+        message(response, is_user = True)
+        message(question)
 
 
-user_input = get_text()
+with st.container():
+    st.text_input("User Response:", on_change=on_input_change, key="user_input")
 
-if user_input:
-    output = query({
-        "inputs": {
-            "past_user_inputs": st.session_state.past,
-            "generated_responses": st.session_state.generated,
-            "text": user_input,
-        },"parameters": {"repetition_penalty": 1.33},
-    })
 
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output["generated_text"])
+# import streamlit as st
+# from streamlit_chat import message
+# import requests
+# from streamlit.components.v1 import html
 
-if st.session_state['generated']:
+# st.set_page_config(
+#     page_title="Streamlit Chat - Demo",
+#     page_icon=":robot:"
+# )
 
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+# API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
+# # headers = {"Authorization": st.secrets['api_key']}
+
+# st.header("Streamlit Chat - Demo")
+# st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = []
+
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
+
+# def query(payload):
+# 	response = requests.post(API_URL, json=payload)	#	headers=headers, 
+# 	return response.json()
+
+# def get_text():
+#     input_text = st.text_input("You: ","Hello, how are you?", key="input")
+#     return input_text 
+
+
+# user_input = get_text()
+
+# if user_input:
+#     output = query({
+#         "inputs": {
+#             "past_user_inputs": st.session_state.past,
+#             "generated_responses": st.session_state.generated,
+#             "text": user_input,
+#         },"parameters": {"repetition_penalty": 1.33},
+#     })
+
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append(output["generated_text"])
+
+# if st.session_state['generated']:
+
+#     for i in range(len(st.session_state['generated'])-1, -1, -1):
+#         message(st.session_state["generated"][i], key=str(i))
+#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
