d193956175a92194446a2d3be1fbbc896951c120
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-28 15:48:51 +0200

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -13,7 +13,7 @@ def on_btn_click():
 st.session_state.setdefault('questions', [])
 
 st.title("Survey QA Bot")
-questions_list = ['q1', 'question 2', 'question 3']
+questions_list = ['question 1', 'question 2', 'question 3']
 
 if 'responses' not in st.session_state.keys():
     st.session_state.questions.extend(questions_list)
@@ -28,10 +28,6 @@ with st.container():
     for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
         message(response, is_user = True)
         message(question)
-
-
-with st.container():
-    st.text_input("User Response:", on_change=on_input_change, key="user_input")
 
 
 with st.container():
