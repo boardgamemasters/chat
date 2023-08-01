e91adff118693a0103d212ebc5846c723527e612
LeilaKarimii <131779385+LeilaKarimii@users.noreply.github.com>
2023-08-01 15:09:40 +0200

    Update test_component.py

--- test_component.py
+++ test_component.py
@@ -1,14 +1,15 @@
+# Import necessary libraries
 from streamlit_chat import message, NO_AVATAR
 import streamlit as st
 
-
+# Define URLs and embedded content
 audio_path = "https://docs.google.com/uc?export=open&id=16QSvoLWNxeqco_Wb2JvzaReSAw5ow6Cl"
 img_path = "https://www.groundzeroweb.com/wp-content/uploads/2017/05/Funny-Cat-Memes-11.jpg"
-youtube_embed = '''
+youtube_embed = ''' # Embedded YouTube video
 <iframe width="400" height="215" src="https://www.youtube.com/embed/LMQ5Gauy17k" title="YouTube video player" frameborder="0" allow="accelerometer; encrypted-media;"></iframe>
 '''
 
-markdown = """
+markdown = """  # Markdown content
 ### HTML in markdown is ~quite~ **unsafe**
 
 <blockquote>
@@ -38,7 +39,7 @@ console.log("Here is some JavaScript code")
 ~~~
 """
 
-table_markdown = '''
+table_markdown = '''  # Table in Markdown format
 A Table:
 
 | Feature     | Support              |
@@ -53,7 +54,7 @@ The term "ChatterBot" was originally coined by Michael Mauldin (creator of the f
 
 [streamlit website](https://streamlit.io)
 """
-
+# Callback functions for user input and clear button click
 def on_input_change():
     user_input = st.session_state.user_input
     st.session_state.past.append(user_input)
@@ -62,8 +63,9 @@ def on_input_change():
 def on_btn_click():
     del st.session_state.past[:]
     del st.session_state.generated[:]
-
+# Main execution
 if __name__ == '__main__':
+    # Initialize past and generated chat messages using session_state.setdefault()
     st.session_state.setdefault(
         'past', 
         ["Hey, \nwhat's a chatbot?", 
@@ -87,20 +89,21 @@ if __name__ == '__main__':
     )
 
     st.title("Chat placeholder")
-
+    # Create an empty container to hold the chat messages
     chat_placeholder = st.empty()
-
+    # Use the container context manager to display chat messages
     with chat_placeholder.container():    
-        for i in range(len(st.session_state['generated'])):                
+        for i in range(len(st.session_state['generated'])): 
+            # Display user input and bot messages using the message function from streamlit_chat
             message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
             message(
                 st.session_state['generated'][i]['data'], 
                 key=f"{i}", 
                 allow_html=True,
                 is_table=st.session_state['generated'][i]['type']=='table',
-                avatar_style=NO_AVATAR
+                avatar_style=NO_AVATAR   # Disable avatar for bot messages
             )
         st.button("Clear message", on_click=on_btn_click)
-
+    # Add a text input box for user input, which triggers the on_input_change() function when changed
     with st.container():
         st.text_input("User Input:", on_change=on_input_change, key="user_input")
